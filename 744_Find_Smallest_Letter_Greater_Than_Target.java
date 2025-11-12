5f8c65aa643d0597e57f82d263c312719d49733d
Aryan Gupta <aryanguptabareilly067@gmail.com>
2025-11-13 01:25:03 +0530

    Update 744_Find_Smallest_Letter_Greater_Than_Target.java

--- 744_Find_Smallest_Letter_Greater_Than_Target.java
+++ 744_Find_Smallest_Letter_Greater_Than_Target.java
@@ -1,31 +1,20 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        if(letters[letters.length - 1] <= target)
-            return letters[0];
-
         int beg = 0, end = letters.length - 1, mid;
-        mid = beg + (end - beg)/2;
+
+        if(target >= letters[letters.length  - 1])
+            return letters[0];
 
         while(beg <= end){
             mid = beg + (end - beg)/2;
 
-            if(letters[mid] < target)
+            if(letters[mid] <= target)
                 beg = mid + 1;
             
-            else if(letters[mid] > target)
+            else
                 end = mid - 1;
-            
-            else{
-                while(letters[mid] == letters[mid + 1])
-                    mid++;
-                return letters[mid + 1];
-            }
         }
-        
-        if(letters[mid] > target)
-            return letters[mid];
-        
-        else
-            return letters[mid + 1];
+
+        return letters[beg %  letters.length];
     }
 }
