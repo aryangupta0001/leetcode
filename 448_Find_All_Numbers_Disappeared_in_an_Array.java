211877b07a5fbaf25ddfbc40114e4b45a86d434f
Aryan Gupta <aryanguptabareilly067@gmail.com>
2025-11-25 02:53:01 +0530

    Create 448_Find_All_Numbers_Disappeared_in_an_Array.java
    Runtime 6 ms Beats 81.34%
    
    Memory 66.95 MB Beats 40.42%

---
+++ 448_Find_All_Numbers_Disappeared_in_an_Array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+
+        int i = 0;
+
+        List<Integer> ans = new ArrayList<>();
+
+        while(i<nums.length){
+            if(nums[i] != nums[nums[i] - 1]){
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+            else
+                i++;
+        }
+
+        for(i = 0; i<nums.length; i++)
+            if(nums[i] != i + 1)
+                ans.add(i+1);
+        
+        return ans;
+    }
+}
