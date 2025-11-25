b84edbd01d1e495a43b418e346baa4ddce117dde
Aryan Gupta <aryanguptabareilly067@gmail.com>
2025-11-26 02:37:28 +0530

    Alternative approach using Cycle Sort
    Runtime 3 ms Beats 69.61%
    
    Memory 47.26 MB Beats 61.82%

--- 645_Set_Mismatch.java
+++ 645_Set_Mismatch.java
@@ -1,25 +1,24 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         int[] ans = new int[2];
-        int i = 0, n = nums.length, a;
+        int i = 0, n = nums.length, a, temp;
 
-        for (i = 0; i < n; i++) {
-            a = Math.abs(nums[i]) - 1;
-
-            if (nums[a] > 0) 
-                nums[a] *= -1;
-            
-            else
-                ans[0] = Math.abs(nums[i]);
-        }
-
-        for(i = 0; i<n; i++)    
-            if(nums[i] > 0){
-                ans[1] = i+1;
-                break;
+        while(i<n)
+            if (nums[i] != nums[nums[i] - 1]) {
+                temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
             }
+            else
+                i++;
 
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1){
+                ans[0] = nums[i];
+                ans[1] = i+1;
+                return ans;
+            }
+        }
         return ans;
-
     }
 }
