ebf738199bcde4cb1502225db69674e1fc68148f
Aryan Gupta <aryanguptabareilly067@gmail.com>
2025-11-27 01:35:11 +0530

    Create 41_First_Missing_Positive.java
    Runtime 1 ms Beats 99.99%
    
    Memory 71.18 MB Beats 26.61%

---
+++ 41_First_Missing_Positive.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int  i = 0, n = nums.length;
+
+        while(i<n)
+            if(nums[i] > 0 && nums[i] < n && nums[i] != nums[nums[i] - 1]){
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+            else
+                i++;
+        
+        for(i = 0; i<n; i++)
+            if(nums[i] != i + 1)
+                return i+1;
+        
+        return i+1;
+    }
+}
