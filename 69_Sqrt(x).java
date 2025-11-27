d59380f5d68350432019168da7890d77fdf43ce5
Aryan Gupta <aryanguptabareilly067@gmail.com>
2025-11-28 01:37:48 +0530

    Update 69_Sqrt(x).java
    Runtime 1 ms Beats 90.99%
    
    Memory 42.66 MB Beat 12.86%

--- 69_Sqrt(x).java
+++ 69_Sqrt(x).java
@@ -1,27 +1,24 @@
 class Solution {
     public int mySqrt(int x) {
-        int a = 0, b = x / 2 + 1;
+        int a = 0, b = x / 2;
 
         if (x == 0 || x == 1)
             return x;
 
-        while (a < b) {
+        while (a <= b) {
             int mid = a + (b - a) / 2;
             long sq = 1L * mid * mid;
 
             if (sq > Integer.MAX_VALUE || (int) sq > x)
-                b = mid;
+                b = mid - 1;
 
             else if (sq == x)
                 return mid;
 
-            else if (sq < x) {
-                if(a == mid)
-                    return a;
-                a = mid;
-            }
+            else if (sq < x)
+                a = mid + 1;
         }
 
-        return a;
+        return b;
     }
 }
