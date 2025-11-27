8bddff7d8bf4e7a63f79bd169351ece96c81e793
Aryan Gupta <aryanguptabareilly067@gmail.com>
2025-11-28 02:38:34 +0530

    Update 167_Two_Sum_II_-_Input_Array_Is_Sorted.java
    Runtime 2 ms Beats 94.68%
    
    Memory 48.12 MB Beats 38.25%

--- 167_Two_Sum_II_-_Input_Array_Is_Sorted.java
+++ 167_Two_Sum_II_-_Input_Array_Is_Sorted.java
@@ -1,39 +1,16 @@
 class Solution {
-
-    public int binarySearch(int[] numbers, int i, int target){
-        int a = i+1, b = numbers.length - 1;
-
-        while(a<=b){
-            int mid = a + (b - a) / 2;
-
-            if(numbers[mid] == target)
-                return mid;
-            
-            else if (numbers[mid] < target)
-                a =  mid + 1;
-            
-            else
-                b = mid - 1;
-        }
-
-        return -1;
-    }
-
     public int[] twoSum(int[] numbers, int target) {
-        int n = numbers.length;
+        int i = 0, j = numbers.length - 1;
 
-        if(n == 2)
-            return new int[] {1, 2};
-
-        for (int i = 0; i < n; i++) {
-            int diff = target - numbers[i];
+        while(i < j)
+            if(numbers[i] + numbers[j] == target)
+                return new int[] {i+1, j+1};
             
-            int index = binarySearch(numbers, i, diff);
-
-            if (index > -1)
-                return new int[] { i + 1, index + 1 };
-        }
-
-        return new int[] { -1, -1 };
+            else if(numbers[i] + numbers[j] < target)
+                i++;
+            
+            else j--;
+        
+        return new int[] {i+1, j+1};
     }
 }
