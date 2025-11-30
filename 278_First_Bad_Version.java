85eeed8cbcd1bd25ab494b50bb0754baab3757a8
Aryan Gupta <aryanguptabareilly067@gmail.com>
2025-12-01 01:09:08 +0530

    Without Caching the Results
    Runtime 13 ms Beats 99.89%
    
    Memory 42.06 MB Beats 19.74%

--- 278_First_Bad_Version.java
+++ 278_First_Bad_Version.java
@@ -3,57 +3,18 @@
 
 public class Solution extends VersionControl {
 
-    boolean isBad(Map<Integer, Boolean> versions, int n){
-        boolean bad  = true;
-        
-        if(versions.containsValue(n))
-            bad = versions.get(n);
-        
-        else
-            bad = isBadVersion(n);
-        
-        return bad;
-
-    }
-
     public int firstBadVersion(int n) {
         int a = 1, b = n;
 
-        Map<Integer, Boolean> versions = new HashMap<>();
-        boolean bad = true;
+        while (a < b) {
+            int mid = a + (b - a) / 2;
 
+            // bad = ;
 
-        while(a<=b){
-            int mid = a + (b-a) / 2;
-
-            bad = isBad(versions, mid);
-            versions.put(mid, bad);
-
-            if(bad){
-                if(mid > 0){   
-                    bad = isBad(versions, mid-1);
-
-                    if(!bad)
-                        return mid;
-                    
-                    else{
-                        versions.put(mid - 1, bad);
-                        b = mid - 2;
-                    }
-                }
-                else
-                    return mid;
-            }
-
-            else{
-                    bad = isBad(versions, mid + 1);
-
-                    if(bad)
-                        return mid + 1;
-                    
-                    else
-                        a = mid  + 2;
-            }
+            if (isBadVersion(mid))
+                b = mid;
+            else
+                a = mid + 1;
 
         }
 
