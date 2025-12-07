ec4f9217e15b5d7859f3673a2059997e59dd48d5
Aryan Gupta <aryanguptabareilly067@gmail.com>
2025-12-08 02:28:14 +0530

    Used Caching to store results to reduce redundant recursive calls
    Runtime 0 ms Beats 100.00%
    
    Memory 42.11 MB Beats 14.00%

--- 509_Fibonacci_Number.java
+++ 509_Fibonacci_Number.java
@@ -1,8 +1,22 @@
 class Solution {
     public int fib(int n) {
-        if(n <= 1)
+        int[] cache = new int[n + 1];
+
+        return fibonacci(n, cache);
+    }
+
+    int fibonacci(int n, int[] cache) {
+
+        if (n <= 1)
             return n;
-        
-        return fib(n-1) + fib(n-2);
+     
+        else if (cache[n] != 0)
+            return cache[n];
+
+        else {
+            cache[n] = fibonacci(n - 1, cache) + fibonacci(n - 2, cache);
+        }
+
+        return cache[n];
     }
 }
