ef74b1058cf15e9b6478e89d87924e22a0e5db48
Aryan Gupta <aryanguptabareilly067@gmail.com>
2025-12-25 02:07:56 +0530

    Recursive Approach instead of Iterative
    Runtime 0 ms Beats 100.00%
    
    Memory 44.13 MB Beats 6.15%

--- 33_Search_in_Rotated_Sorted_Array.java
+++ 33_Search_in_Rotated_Sorted_Array.java
@@ -1,54 +1,48 @@
 class Solution {
     public int search(int[] nums, int target) {
+        int beg = 0, end = nums.length - 1;
 
-        int beg = 0, end = nums.length - 1, pivot = end;
+        int pivot = findPivot(nums, beg, end);
 
-        while (beg <= end) {
+        if (target == nums[pivot])
+            return pivot;
+
+        else if (target < nums[beg])
+            return getIndex(nums, pivot + 1, end, target);
+
+        return getIndex(nums, beg, pivot, target);
+    }
+
+    static int findPivot(int[] nums, int beg, int end) {
+        int mid = beg + (end - beg) / 2;
+
+        if (mid < end && nums[mid] > nums[mid + 1])
+            return mid;
+
+        else if (mid > 0 && nums[mid] < nums[mid - 1])
+            return mid - 1;
+
+        else if (nums[mid] < nums[beg])
+            return findPivot(nums, beg, mid - 1);
+
+        else if (mid < end && nums[mid] < nums[mid + 1])
+            return findPivot(nums, mid + 1, end);
+
+        return nums.length - 1;
+    }
+
+    static int getIndex(int[] nums, int beg, int end, int target) {
+        if (beg <= end) {
             int mid = beg + (end - beg) / 2;
 
-            if (mid < end && nums[mid] > nums[mid + 1]) {
-                pivot = mid;
-                break;
-            }
+            if (nums[mid] == target)
+                return mid;
 
-            if(mid > beg && nums[mid] < nums[mid - 1]){
-                pivot = mid - 1;
-                break;
-            }
+            else if (nums[mid] < target)
+                return getIndex(nums, mid + 1, end, target);
 
-            if(nums[mid] < nums[beg])
-                end = mid - 1;
-            
             else
-                beg = mid + 1;
-        }
-
-        if(target == nums[pivot])
-            return pivot;
-            
-        else{
-            if(target >= nums[0]){
-                beg = 0;
-                end = pivot;
-            }
-
-            else{
-                beg = pivot + 1;
-                end = nums.length - 1;
-            }
-
-            while(beg <= end){
-                int mid = beg + (end - beg) / 2;
-
-                if(target == nums[mid])
-                    return mid;
-                
-                else if(target < nums[mid])
-                    end = mid - 1;
-                
-                else
-                    beg = mid + 1;
-            }
+                return getIndex(nums, beg, mid - 1, target);
         }
         return -1;
     }
