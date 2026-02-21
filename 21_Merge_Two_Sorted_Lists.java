57635db202d99804d34ed1f237f9696ca9c0991a
Aryan Gupta <aryanguptabareilly067@gmail.com>
2026-02-22 00:54:07 +0530

    Removed redundant  if-condition
    Runtime 0 ms Beats 100.00%
    
    Memory 44.17 MB Beats 88.69%

--- 21_Merge_Two_Sorted_Lists.java
+++ 21_Merge_Two_Sorted_Lists.java
@@ -2,28 +2,17 @@ class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         return merged(list1, list2);
     }
- 
+
     public static ListNode merged(ListNode list1, ListNode list2) {
-        if (list1 != null) {
-            if (list2 != null) {
-                if (list1.val < list2.val) {
-                    list1.next = merged(list1.next, list2);
-                    return list1;
-                }
-
-                else if (list1.val == list2.val) {
-                    list1.next = merged(list1.next, list2);
-                    return list1;
-                }
-
-                else {
-                    list2.next = merged(list1, list2.next);
-                    return list2;
-                }
+        if (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                list1.next = merged(list1.next, list2);
+                return list1;
             }
-            return list1;
 
+            list2.next = merged(list1, list2.next);
+            return list2;
         }
-        return list2;
+        return list1 != null ? list1 : list2;
     }
 }
